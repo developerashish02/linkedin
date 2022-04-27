67b1ab63ee9306192a6df5a7152713af98936266
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-04-27 14:13:15 +0530

    ADD Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import Styled from 'styled-components';
+import React from "react";
+import "../styles/Login.css";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>Hello Ashish </h1>
-        </div>
-    )
-}
+	return (
+		<div className="login__container">
+			<div className="login__nav">
+				<a href="/">
+					<img src="/images/login-logo.svg" alt="logo" />
+				</a>
 
-export default Login; 
+				<div>
+					<a className="login__join">Join now </a>
+					<a className="login__signUp"> Sign In</a>
+				</div>
+			</div>
+
+			<section className="login__section">
+				<div className="login__hero">
+					<h1>welcome to your professional community</h1>
+
+					<img src="/images/login-hero.svg" alt="logo hero image" />
+				</div>
+
+				<div className="login__form">
+					<button className="login__google">
+						<img src="/images/google.svg" alt="" /> sign in with google
+					</button>
+				</div>
+			</section>
+		</div>
+	);
+};
+
+export default Login;
