b87ac70d3de674878b222fa629b95b8864328b63
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-04-28 15:38:05 +0530

    import And export and fix styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,32 @@
 import React from "react";
-import "../styles/Home.css"; 
+import "../styles/Home.css";
+import Leftside from "./Leftside";
+import Rightside from "./Rightside";
+import Mainside from "./Mainside";
 
 const Home = () => {
-    return (
-        <div className="home">
-            <h1>Hello i am a home</h1>
-        </div>
-    )
-}
+	return (
+		<div className="home__container">
+			<div className="home__content">
+				<section className="home__section">
+					<a>
+						<h5>Hiring in a hyrry? - </h5>
+					</a>
+					<p>
+						Find talented pros in record time with Upwork and keep business
+						Moving
+					</p>
+				</section>
 
-export default Home;  
+				{/* layout */}
+				<section className="home__layout">
+					{<Leftside />}
+					{<Mainside />}
+					{<Rightside />}
+				</section>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
