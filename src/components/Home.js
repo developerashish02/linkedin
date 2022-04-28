c5500f07ec3b263f61e678274ca95c8b68d21cd4
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-04-28 10:25:40 +0530

    create file home and home css for routing

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Home.css"; 
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>Hello i am a home</h1>
+        </div>
+    )
+}
+
+export default Home;  
