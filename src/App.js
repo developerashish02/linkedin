5f9f2c52f9ddbc5a5bdb9ce1afdc37b9ff15f8a6
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-04-28 10:28:23 +0530

    import and export

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-
-import './App.css';
-import Reatc from 'react';
-import { BrowserRouter as Router, Routes, Link, Route } from 'react-router-dom';
-import Login from './components/Login';
+import "./App.css";
+import Reatc from "react";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Routes, Link, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path='/' element={<Login />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Header />
+				<Routes>
+					<Route exact path="/" element={<Login />} />
+					<Route path="/home" element={<Home />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
