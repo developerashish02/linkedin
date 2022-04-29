9d9908aaae8ef07af32f2b7fa7ba9899bde5d3b0
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-04-29 17:09:24 +0530

    ADD Image and Avtar is Added

---
+++ src/components/Icon.js
@@ -0,0 +1,22 @@
+import InsertPhotoIcon from "@mui/icons-material/InsertPhoto";
+import MusicVideoIcon from "@mui/icons-material/MusicVideo";
+import EventIcon from "@mui/icons-material/Event";
+import ArticleIcon from "@mui/icons-material/Article";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import CommentIcon from "@mui/icons-material/Comment";
+import ShareIcon from "@mui/icons-material/Share";
+import SendIcon from "@mui/icons-material/Send";
+
+
+export {
+    InsertPhotoIcon,
+    MusicVideoIcon,
+    EventIcon,
+    ArticleIcon,
+    ThumbUpIcon,
+    MoreHorizIcon,
+    CommentIcon,
+    ShareIcon,
+    SendIcon,
+}
