b47a4cab03e0ef4607e437736cb81059f9449684
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-04-29 11:26:29 +0530

    Remove all css styles file and add style components in project and ADD Right side component and its style

---
+++ src/components/Mainside.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Mainside = () => {
+    return (
+        <div className="Mainside__conatiner">
+            
+        </div>
+    )
+}
+
+export default Mainside; 
