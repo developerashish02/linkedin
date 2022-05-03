aac39dde6d8476b33e73a9c96a7275025b6e9211
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-05-03 18:36:21 +0530

    set user profile img using firebase email URL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
-import Reatc from "react";
+import Reatc, { useEffect } from "react";
 import Header from "./components/Header";
 import { BrowserRouter as Router, Routes, Link, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
-function App() {
+import {getUserAuth} from './actions'
+import { connect } from "react-redux";
+function App(props) {
+	useEffect(() => {
+		props.getUserAuth(); 
+	}, []); 
 	return (
 		<div className="App">
 			<Router>
@@ -18,4 +23,12 @@ function App() {
 	);
 }
 
-export default App;
+const mapStateToProps = (state) => {
+	return {}; 
+}
+
+const mapDispatchToProps = (dispatch) => ({
+	getUserAuth: () => dispatch(getUserAuth()) 
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);  
